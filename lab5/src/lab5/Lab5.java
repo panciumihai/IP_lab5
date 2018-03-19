1d295dc2b03145e228f4c2143f0f7686628cd9be
paulyks <p4uLycS@yahoo.com>
2018-03-19 21:06:26 +0200

    ALT + SHIFT + F

--- lab5/src/lab5/Lab5.java
+++ lab5/src/lab5/Lab5.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab5;
 
-/**
- *
- * @author Mihai
- */
 public class Lab5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("we are dead. i'm just kidding");
-        // TODO code application logic here
+        System.out.println("ALT + SHIFT + F for auto-indenting ;)");
     }
-    
+
 }
