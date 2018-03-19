aeee1b8883139a53a9fc3a2f8d4033dc4092e807
UngureanAlexandruFlorin <ungurean.alexandru.florin@gmail.com>
2018-03-19 21:14:08 +0200

    ...

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -15,7 +15,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Merge !!!");
+        System.out.println("IntelIJ este mai bun!!!");
     }
     
 }
